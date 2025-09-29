613f175f03a2fe9e48e1359cc8858d18df5547f5
Frank Steinmetz <frank.steinmetz@steinmetz-it.de>
2025-09-29 20:31:42 +0200

    Header angelegt

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  /*
+  const testdaten = [
+    { id: 1, date: "01.09.2025", message: "Text vom 01.09.2025" },
+    { id: 2, date: "02.09.2025", message: "Text vom 02.09.2025" },
+    { id: 3, date: "03.09.2025", message: "Text vom 03.09.2025" },
+    { id: 4, date: "04.09.2025", message: "Text vom 04.09.2025" },
+  ];
+
+  function initData() {
+    console.log("initData");
+    localStorage.setItem("PersonalDiary", JSON.stringify(testdaten));
+  }
+
+  initData();
+  */
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="">
+        <Header />
+        <div className="">
+          <button onClick="alert(Click)}" className="">
+            Add new entry
+          </button>
+        </div>
+        <div className="card"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="">
+        <h1 className="">Personal Diary</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
